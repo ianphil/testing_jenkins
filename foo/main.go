41e159aa93e7afa568c83ff4f406a338b8c06d95
Ian Philpot <ian.philpot@microsoft.com>
2018-10-21 19:34:13 -0400

    git diff

--- foo/main.go
+++ foo/main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	// test out of sample - foo single - poll
+	// test out of sample - foo single - poll- git diff
 }
 
 func main() {
