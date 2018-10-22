e9dc801a0f8eb974b21786a1d800da39849363c5
Ian Philpot <ian.philpot@microsoft.com>
2018-10-21 20:15:39 -0400

    bar push 7

--- foo/main.go
+++ foo/main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	// test out of sample - foo single - poll- git diff - diff - diff
+	// test out of sample - foo single - poll- git diff - diff - diff - diff
 }
 
 func main() {
